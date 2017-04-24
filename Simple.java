4a868f6b8923aa73dc7d3a65c03567c55d8b9b1f
Sangeeth <sarasangeetharajang@gmail.com>
2017-04-24 19:54:53 +0530

    sdf

--- Simple.java
+++ Simple.java
@@ -11,6 +11,5 @@ public static void main(String args[])
      System.out.println("Hello Java"); 
 =======
      System.out.println("Hello Java");  
->>>>>>> 125358d445aba6dd1bb72bebbaf65cd3cf173591
-    }  
+>>>>>>    }  
 }
